c88514f37c64d609916c5aaec89a6fbb187adfd6
Rodrigo Gai <rcrosse@walmart.com>
2016-01-20 23:13:20 -0200

    Remove Meta::RefresMethoValues

--- lib/meta/refresh_method_values.rb
+++
@@ -1,9 +0,0 @@
-module Meta
-  class RefreshMethodValues
-    def refresh(options = {})
-      defaults[:memory_size] = 100
-
-      self.memory_size = options[:memory_size] if options[:memory_size]
-    end
-  end
-end
